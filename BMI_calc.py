eba630e3aff3f67fabce0582dc869be0fe7be1d5
JustKnowing00 <115400868+JustKnowing00@users.noreply.github.com>
2024-12-09 14:52:00 +0530

    BMI Calculator
    Calculates the BMI of a person by taking into account the system (imperial/metric), age, gender, height, and weight. also provides a visual representation of the calculated values.

---
+++ BMI_calc.py
@@ -0,0 +1,191 @@
+from colorama import Fore, init, Style
+import time
+
+bmi = 0
+BMR = 0
+LBM = 0
+body_fat = 0
+
+print("Welcome to BMI calculator !")
+print("Please select the unit of calculation: \n 1> Imperial \n 2> Metric")
+mode = int(input("Select option: "))
+while mode != 1 and mode != 2:
+    print("Select appropriate option.")
+    print("Please select the unit of calculation: \n 1> Imperial \n 2> Metric")
+    mode = int(input("Select option: "))
+
+def bmi_bar(bmi_mapped, ranges, colors, length=100):
+    bar = ""
+    for i in range(1, length + 1):
+        if i == bmi_mapped:
+            bar += "█"  
+        else:
+            for (start, end), color in zip(ranges, colors):
+                if start <= i <= end:
+                    bar += color + "="
+                    break
+    return bar + Fore.RESET
+
+def fat_bar(gender, fat, low, high, bar_length=100):
+    bar = ""
+    
+    for i in range(1, bar_length + 1):
+        if i == fat:
+            bar += "█"  
+        elif i < low or i > high:
+            bar += Fore.WHITE + "="
+        else:
+            if gender== 1:
+                bar += Fore.CYAN + "="
+            else:
+                bar += Fore.MAGENTA + "="
+    return bar + Fore.RESET
+
+def LBM_bar(gender, LBM, low, high, bar_length=100):
+    bar = ""
+    
+    for i in range(1, bar_length + 1):
+        if i == LBM:
+            bar += "█"  
+        elif i < low or i > high:
+            bar += Fore.WHITE + "="
+        else:
+            if gender== 1:
+                bar += Fore.CYAN + "="
+            else:
+                bar += Fore.MAGENTA + "="
+    return bar + Fore.RESET
+
+def FFMI_bar(FFMI, low, high, bar_length=100):
+    bar = ""
+    
+    for i in range(1, bar_length + 1):
+        if i == LBM:
+            bar += "█"  
+        elif i < low or i > high:
+            bar += Fore.WHITE + "="
+        else:
+            bar += Fore.GREEN + "="
+
+    return bar + Fore.RESET
+
+def visual(age, gender, bmi, LBM, FFMI, body_fat, BMR):
+    
+    bmi_mapped = round(100*bmi/60)
+    fat = round(body_fat)
+    if mode == 1:
+        unit = "lbs"
+    else:
+        unit = "kg"
+    print(" ")
+
+    #-------------------------------------------BMI---------------------------------------------------
+
+    if age < 18:
+        ranges = [(1, 5), (6, 85), (86, 95), (96, 100)]
+        colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.RED]
+    elif age < 65:
+        ranges = [(1, 28), (29, 31), (32, 42), (43, 60), (61, 100)]
+        colors = [Fore.RED, Fore.YELLOW, Fore.GREEN, Fore.YELLOW, Fore.RED]
+    else:
+        ranges = [(1, 38), (39, 43), (44, 50), (51, 58), (59, 100)]
+        colors = [Fore.RED, Fore.YELLOW, Fore.GREEN, Fore.YELLOW, Fore.RED]
+
+    disp = bmi_bar(bmi_mapped, ranges, colors)
+    print(f"BMI : {bmi:.2f} kg/m^2: " + disp)
+
+    #----------------------------------------BODY FAT PERCENTAGE-----------------------------------------------
+
+    if gender == 1: 
+        if age < 18:
+            print(f"Body Fat : {body_fat:.2f} % : " + fat_bar(1, round(body_fat), 10, 20))
+        elif age < 65:
+            print(f"Body Fat : {body_fat:.2f} % : " + fat_bar(1, round(body_fat), 10, 25))
+        else:
+            print(f"Body Fat : {body_fat:.2f} % : " + fat_bar(1, round(body_fat), 12, 26))
+    
+    else:
+        if age < 18:
+            print(f"Body Fat : {body_fat:.2f} % : " + fat_bar(2, round(body_fat), 15, 25))
+        elif age < 65:
+            print(f"Body Fat : {body_fat:.2f} % : " + fat_bar(2, round(body_fat), 18, 30))
+        else:
+            print(f"Body Fat : {body_fat:.2f} % : " + fat_bar(2, round(body_fat), 20, 33))
+    
+    #------------------------------------------LEAN BODY MASS------------------------------------------------------
+
+    if gender == 1: 
+        if age < 18:
+            print(f"Lean Body Mass : {LBM:.2f} {unit} : " + fat_bar(1, round(LBM), 65, 80))
+        elif age < 65:
+            print(f"Lean Body Mass : {LBM:.2f} {unit} : " + fat_bar(1, round(LBM), 70, 85))
+        else:
+            print(f"Lean Body Mass : {LBM:.2f} {unit} : " + fat_bar(1, round(LBM), 65, 80))
+    
+    else:
+        if age < 18:
+            print(f"Lean Body Mass : {LBM:.2f} {unit} : " + fat_bar(2, round(LBM), 80, 75))
+        elif age < 65:
+            print(f"Lean Body Mass : {LBM:.2f} {unit} : " + fat_bar(2, round(LBM), 60, 80))
+        else:
+            print(f"Lean Body Mass : {LBM:.2f} {unit} : " + fat_bar(2, round(LBM), 55, 75))
+
+    #------------------------------------------FAT FREE MASS INDEX----------------------------------------------------
+
+    if age < 18:
+        print(f"Fat Free Mass Index : {FFMI:.2f} : " + fat_bar(1, round(LBM), 16, 21))
+    elif age < 65:
+        print(f"Fat Free Mass Index : {FFMI:.2f} : " + fat_bar(1, round(LBM), 18, 22))
+    else:
+        print(f"Fat Free Mass Index : {FFMI:.2f} : " + fat_bar(1, round(LBM), 16, 20))
+
+
+def sex():
+    print("Please select your gender: \n 1> Male \n 2> Female")
+    gender = int(input("Select option: "))
+    while gender != 1 and gender != 2 and gender != 3:
+        print("Select appropriate option.")
+        print("Please select your gender: \n 1> Male \n 2> Female ")
+        gender = int(input("Select option: "))
+    return gender
+
+def imperial():
+    age = int(input("Enter your Age: "))
+    gender = sex()
+    height_feet = int(input("Enter your height in feet: "))
+    height_inches = int(input("Enter your height in inches: "))
+    weight = float(input("Enter your weight: "))
+    height = height_feet/12 + height_inches
+    bmi = (weight*703 / (height ** 2))
+
+    if gender == 1: 
+        body_fat = (1.20 * bmi) + (0.23 * age) - 16.2
+    else:
+        body_fat = (1.20 * bmi) + (0.23 * age) - 5.4
+    
+    LBM = weight * (1- body_fat/100)
+    FFMI = LBM / ((height * 0.0254) ** 2)
+
+    visual(age, gender, bmi, LBM, FFMI, body_fat, BMR)
+
+def metric():
+    age = int(input("Enter your Age: "))
+    gender = sex()
+    height = int(input("Enter your height in centimeters: "))
+    weight = float(input("Enter your weight: "))
+    bmi = (weight / ((height/100) ** 2))
+
+    if gender == 1: 
+        body_fat = (1.20 * bmi) + (0.23 * age) - 16.2
+    else:
+        body_fat = (1.20 * bmi) + (0.23 * age) - 5.4
+
+    LBM = weight * (1- body_fat/100)
+    FFMI = LBM / (height ** 2)
+
+    visual(age, gender, bmi, LBM, FFMI, body_fat, BMR)
+
+if mode == 1:
+    imperial()
+else:
+    metric()
